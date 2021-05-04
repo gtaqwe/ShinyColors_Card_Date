95249aae7975f123869a5c55afcd0f2973bab471
ZEED <gtaqwe@gmail.com>
2021-05-04 22:45:01 +0900

    Add files via upload

---
+++ CreateTable.js
@@ -0,0 +1,294 @@
+function buildTable(jsonData) {
+    runBuildTable(jsonData)
+    runBuildDateRank()
+}
+
+function runBuildTable(jsonData) {
+    var tableTitle = jsonData[0].header_title
+    var rowLength = jsonData.length
+    var columnLength = jsonData[0].header_length
+
+    var table = '<table id="date-table">';
+    for (var singleRow = 0; singleRow < rowLength; singleRow++) {
+        if (singleRow === 0) {
+            table += '<thead>';
+            table += '<tr>';
+        } else {
+            table += '<tr>';
+        }
+        if (singleRow === 0) {
+            table += tableHeader(tableTitle, columnLength);
+        } else {
+            table += calTerm(jsonData[singleRow])
+        }
+        if (singleRow === 0) {
+            table += '</tr>';
+            table += '</thead>';
+            table += '<tbody>';
+        } else {
+            table += '</tr>';
+        }
+    }
+    table += '</tbody>';
+    table += '</table>';
+
+    document.getElementById('MAIN').innerHTML = table;
+
+    imgMapping(this);
+};
+
+function tableHeader(title, len) {
+    var resContent = '<th class="th-name-cell" id="table-type">' + title + '</th>';
+
+    for (var i = 0; i < len; i++) {
+        cellTitle = i;
+        if (i == 0) cellTitle = '첫 실장'
+        resContent += '<th>' + cellTitle + '</th>';
+        resContent += '<th class="pre-term">' + '간격' + '</th>';
+    }
+
+    return resContent
+}
+
+function calTerm(totalData) {
+    var resContent = '<td class="td-name-cell">' + totalData.idol_name + '</td>';
+
+    cardDataList = totalData.card_data
+    len = totalData.card_data.length
+    for (var idx = 0; idx < len; idx++) {
+        var date1;
+        var date2;
+        var currDay = 24 * 60 * 60 * 1000;
+        var term;
+        var termCode;
+
+        var cardDate = cardDataList[idx].card_date;
+
+        var cardType = cardDataList[idx].card_type;
+        var cardName = cardDataList[idx].card_name;
+        var cardAddr = cardDataList[idx].card_addr;
+
+        if (cardType == "한정") {
+            resContent += '<td class="limit-card-cell" ';
+        }
+        else if (cardType == "이벤트") {
+            resContent += '<td class="event-card-cell" ';
+        }
+        else if (cardType == "캠페인") {
+            resContent += '<td class="campaign-card-cell" ';
+        }
+        else {
+            resContent += '<td ';
+        }
+
+        resContent += 'addr="' + cardAddr + '" name="' + cardName + '"' + '>' + cardDate + '</td>';
+
+        if (cardDate != '') {
+            date1 = calDate(cardDate);
+
+            if (idx == len - 1 || cardDataList[idx + 1].card_date == '') {
+                date2 = calDate(getDate());
+                term = (date2 - date1) / currDay;
+                termCode = '<td class="now-term">' + term + '</td>';
+            }
+            else {
+                date2 = calDate(cardDataList[idx + 1].card_date);
+                term = (date2 - date1) / currDay;
+                termCode = '<td class="pre-term">' + term + '</td>';
+            }
+        }
+        else {
+            term = '';
+            termCode = '<td>' + term + '</td>';
+        }
+        resContent = resContent + termCode;
+    }
+
+    return resContent;
+}
+
+function getDate() {
+    var targetDate = document.getElementById('TargetDate').valueAsDate;
+    var nowYear = targetDate.getFullYear();
+    var nowMonth = targetDate.getMonth() + 1;
+    var nowDate = targetDate.getDate();
+
+    return nowYear.toString() + '-' + nowMonth.toString().padStart(2, '0') + '-' + nowDate.toString();
+}
+
+function calDate(dateStr) {
+    var dateAry = dateStr.split('-');
+    var y = dateAry[0] - 1900;
+    var m = dateAry[1] - 1;
+    var d = dateAry[2];
+    var date = new Date(y, m, d);
+
+    return date;
+}
+
+function runBuildDateRank() {
+    var targetTable = document.getElementById('date-table');
+    var termAry = [];
+
+    var names = targetTable.getElementsByClassName('td-name-cell');
+    var terms = targetTable.getElementsByClassName('now-term');
+
+    for (var i = 0; i < names.length; i++) {
+        nameStr = names[i].innerHTML
+
+        if (terms[i] === undefined) {
+            termAry.push([nameStr, ""])
+        }
+        else {
+            termAry.push([nameStr, terms[i].innerHTML])
+        }
+
+    }
+
+    buildDataRank(termAry);
+}
+
+function buildDataRank(termAry) {
+    var oldFirst = termAry.slice().sort(function (a, b) { return b[1] - a[1] });
+    // var newFirst = termAry.slice().sort(function(a,b){return a-b});
+    var oldRanks = termAry.map(function (v) { return oldFirst.indexOf(v) + 1 });
+    // var newRanks = termAry.map(function(v){ return newFirst.indexOf(v)+1 });
+
+    var borderStyle = {
+        'left': 'border-left: 2px solid #000000;',
+        'top': 'border-top: hidden;',
+        'bottom': 'border-bottom: hidden;'
+    }
+
+    var tableType = document.getElementById('table-type').innerHTML
+
+    buildRankTable0(tableType, termAry, oldRanks, borderStyle);
+    // buildRankTable1(tableType, termAry, oldRanks, borderStyle);
+    buildRankTable2(tableType, termAry, oldRanks, borderStyle);
+
+}
+
+function buildRankTable0(table, termAry, oldRanks, borderStyle) {
+    var table = $('#date-table');
+    var tableRows = table.find('tr')
+
+    $(tableRows[0]).append('<th style="' + borderStyle.left + borderStyle.top + borderStyle.bottom + '"></th>');
+    $(tableRows[0]).append('<th style="' + borderStyle.left + '">이름</th>');
+    $(tableRows[0]).append('<th style="' + borderStyle.left + '">순위</th>');
+    $(tableRows[0]).append('<th style="' + borderStyle.left + '">간격일</th>');
+
+    for (var i = 0; i < termAry.length; i++) {
+        var rankStr = oldRanks[i]
+        var nameStr = termAry[i][0]
+        var termStr = termAry[i][1]
+        var tableIndex = i + 1
+
+        var styleStr = borderStyle.left
+
+        if (termAry[i] == '') rankStr = '미실장';
+
+        if (rankStr == '1') styleStr += 'background-color: rgba(255, 0, 0, 0.4) ; '; // Red
+        else if (rankStr == '2') styleStr += 'background-color: rgba(255, 165, 0, 0.4) ; '; // Orange
+        else if (rankStr == '3') styleStr += 'background-color: rgba(255, 255, 0, 0.4) ; '; // Yellow
+        else if (rankStr == '4') styleStr += 'background-color: rgba(0, 128, 0, 0.4) ; '; // Green
+        else if (rankStr == '5') styleStr += 'background-color: rgba(0, 0, 255, 0.4) ; '; // Blue
+        else if (rankStr == '6') styleStr += 'background-color: rgba(75, 0, 130, 0.4) ; '; // Indigo
+        else if (rankStr == '7') styleStr += 'background-color: rgba(238, 130, 238, 0.4) ; '; // Violet
+
+        $(tableRows[tableIndex]).append('<td style="' + borderStyle.left + borderStyle.bottom + '"></td>');
+        $(tableRows[tableIndex]).append('<td style="' + styleStr + '">' + nameStr + '</td>');
+        $(tableRows[tableIndex]).append('<td style="' + styleStr + '">' + rankStr + '</td>');
+        $(tableRows[tableIndex]).append('<td style="' + styleStr + '">' + termStr + '</td>');
+    }
+}
+
+function buildRankTable1(tableType, termAry, oldRanks, borderStyle) {
+    var table = '<table id="rank-table-1">';
+    table += '<thead>';
+    table += '<tr>';
+    table += '<th colspan="3">' + tableType + '</th>';
+    table += '</tr>';
+
+    table += '<tr>';
+    table += '<th style="' + borderStyle.left + '">이름</th>';
+    table += '<th style="' + borderStyle.left + '">순위</th>';
+    table += '<th style="' + borderStyle.left + '">간격일</th>';
+    table += '</tr>';
+    table += '</thead>';
+
+    table += '<tbody>';
+
+    for (var i = 0; i < termAry.length; i++) {
+        var rankStr = oldRanks[i];
+        var nameStr = termAry[i][0];
+        var termStr = termAry[i][1];
+
+        var styleStr = borderStyle.left;
+
+        if (termAry[i] == '') rankStr = '미실장';
+
+        if (rankStr == '1') styleStr += 'background-color: rgba(255, 0, 0, 0.4) ; '; // Red
+        else if (rankStr == '2') styleStr += 'background-color: rgba(255, 165, 0, 0.4) ; '; // Orange
+        else if (rankStr == '3') styleStr += 'background-color: rgba(255, 255, 0, 0.4) ; '; // Yellow
+        else if (rankStr == '4') styleStr += 'background-color: rgba(0, 128, 0, 0.4) ; '; // Green
+        else if (rankStr == '5') styleStr += 'background-color: rgba(0, 0, 255, 0.4) ; '; // Blue
+        else if (rankStr == '6') styleStr += 'background-color: rgba(75, 0, 130, 0.4) ; '; // Indigo
+        else if (rankStr == '7') styleStr += 'background-color: rgba(238, 130, 238, 0.4) ; '; // Violet
+
+        table += '<tr>';
+        table += '<td style="' + styleStr + '">' + nameStr + '</td>';
+        table += '<td style="' + styleStr + '">' + rankStr + '</td>';
+        table += '<td style="' + styleStr + '">' + termStr + '</td>';
+        table += '</tr>';
+    }
+
+    table += '</tbody>';
+    table += '</table>';
+    document.getElementById('RANK1').innerHTML = table;
+}
+
+function buildRankTable2(tableType, termAry, oldRanks, borderStyle) {
+    var table = '<table id="rank-table-2">';
+    table += '<thead>';
+    table += '<tr>';
+    table += '<th colspan="3">' + tableType + '</th>';
+    table += '</tr>';
+
+    table += '<tr>';
+    table += '<th style="' + borderStyle.left + '">이름</th>';
+    table += '<th style="' + borderStyle.left + '">순위</th>';
+    table += '<th style="' + borderStyle.left + '">간격일</th>';
+    table += '</tr>';
+    table += '</thead>';
+
+    table += '<tbody>';
+
+    for (var i = 0; i < termAry.length; i++) {
+        var rankIndex = oldRanks.indexOf(i + 1);
+        var rankStr = oldRanks[rankIndex];
+        var nameStr = termAry[rankIndex][0];
+        var termStr = termAry[rankIndex][1];
+
+        var styleStr = borderStyle.left;
+
+        if (termAry[rankIndex] == '') rankStr = '미실장';
+
+        if (rankStr == '1') styleStr += 'background-color: rgba(255, 0, 0, 0.4) ; '; // Red
+        else if (rankStr == '2') styleStr += 'background-color: rgba(255, 165, 0, 0.4) ; '; // Orange
+        else if (rankStr == '3') styleStr += 'background-color: rgba(255, 255, 0, 0.4) ; '; // Yellow
+        else if (rankStr == '4') styleStr += 'background-color: rgba(0, 128, 0, 0.4) ; '; // Green
+        else if (rankStr == '5') styleStr += 'background-color: rgba(0, 0, 255, 0.4) ; '; // Blue
+        else if (rankStr == '6') styleStr += 'background-color: rgba(75, 0, 130, 0.4) ; '; // Indigo
+        else if (rankStr == '7') styleStr += 'background-color: rgba(238, 130, 238, 0.4) ; '; // Violet
+
+        table += '<tr>';
+        table += '<td style="' + styleStr + '">' + nameStr + '</td>';
+        table += '<td style="' + styleStr + '">' + rankStr + '</td>';
+        table += '<td style="' + styleStr + '">' + termStr + '</td>';
+        table += '</tr>';
+    }
+
+    table += '</tbody>';
+    table += '</table>';
+    document.getElementById('RANK').innerHTML = table;
+}
---
+++ IndexScripts.js
@@ -0,0 +1,251 @@
+var nowSelect = 0;
+var jsonData;
+
+document.body.addEventListener("onload", init());
+
+async function init() {
+    await getJSON("json/data.json").then(function(resp) {
+        jsonData = JSON.parse(resp);
+    });
+    // console.log("jsonData:",jsonData);
+
+    nowSelect = 0;
+    document.getElementById("TargetDate").valueAsDate = new Date(getToday());
+
+    var fesChk = $('#fesImgConvertBtn').is(':checked');
+    getToggleString(fesChk);
+}
+
+function getJSON(jsonFile) {
+    try {
+        return new Promise(function (resolve, reject) {
+            var request = new XMLHttpRequest();
+            request.open('GET', jsonFile, true);
+            request.onload = function () {
+                if (request.status == 200) {
+                    resolve(request.responseText);
+                } else {
+                    reject(Error(request.statusText));
+                }
+            };
+
+            request.onerror = function () {
+                reject(Error('Error fetching data.'));
+            };
+            request.send();
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// function loadJSON() {
+//     var xmlhttp = new XMLHttpRequest();
+//     var url = "./test.json";
+
+//     xmlhttp.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             jsonData = JSON.parse(xmlhttp.responseText);
+//         }
+//     };
+//     xmlhttp.open("GET", url, true);
+//     xmlhttp.send();
+// }
+
+function getToday() {
+    var today = new Date();
+    var nowYear = today.getFullYear();
+    var nowMonth = today.getMonth() + 1;
+    var nowDate = today.getDate();
+
+    return nowYear.toString() + '-' + nowMonth.toString().padStart(2, '0') + '-' + nowDate.toString().padStart(2, '0');
+}
+
+function updateDate(nowSelect) {
+    if (nowSelect == 1) {
+        P_SSR();
+    }
+    else if (nowSelect == 2) {
+        S_SSR();
+    }
+    else if (nowSelect == 3) {
+        P_SR();
+    }
+    else if (nowSelect == 4) {
+        S_SR();
+    }
+}
+
+function getSelectVal() {
+    var val
+    if (nowSelect == 1 || nowSelect == 3) {
+        val = "p";
+    }
+    else if (nowSelect == 2 || nowSelect == 4) {
+        val = "s";
+    }
+    return val;
+}
+
+function getToggleString(fesChk) {
+    if (fesChk == true) {
+        $("#toggleStr").html("페스");
+    }
+    else {
+        $("#toggleStr").html("사복");
+    }
+}
+
+function CtlfesImgConvertBtn(ps) {
+    if (ps == "p") {
+        document.getElementById("fesImgConvertBtn").disabled = false;
+    }
+    else if (ps == "s") {
+        document.getElementById("fesImgConvertBtn").checked = false;
+        document.getElementById("fesImgConvertBtn").disabled = true;
+        getToggleString(false);
+    }
+
+}
+
+function P_SSR() {
+    buildTable(jsonData.P_SSR);
+
+    document.getElementById('NOTE_SPACE').innerText = '※ P카드의 첫 실장일은 「白いツバサ」 실장일';
+    nowSelect = 1;
+
+    CtlfesImgConvertBtn("p");
+}
+
+function P_SR() {
+    buildTable(jsonData.P_SR);
+
+    document.getElementById('NOTE_SPACE').innerText = '※ P카드의 첫 실장일은 「白いツバサ」 실장일';
+    nowSelect = 3;
+
+    CtlfesImgConvertBtn("p");
+}
+
+function S_SSR() {
+    buildTable(jsonData.S_SSR);
+
+    document.getElementById('NOTE_SPACE').innerText = '※ S카드의 첫 실장일은 「283プロのヒナ」 실장일';
+    nowSelect = 2;
+
+    CtlfesImgConvertBtn("s");
+}
+
+function S_SR() {
+    buildTable(jsonData.S_SR);
+
+    document.getElementById('NOTE_SPACE').innerText = '※ S카드의 첫 실장일은 「283プロのヒナ」 실장일';
+    nowSelect = 4;
+
+    CtlfesImgConvertBtn("s");
+}
+
+//////////////////////////////////////////////////
+/*
+* Image preview script 
+* powered by jQuery (http://www.jquery.com)
+* 
+* written by Alen Grakalic (http://cssglobe.com)
+* 
+* for more info visit http://cssglobe.com/post/1695/easiest-tooltip-and-image-preview-using-jquery
+*
+*/
+
+function imgMapping() {
+
+    /* CONFIG */
+
+    var xOffset = 10;
+    var yOffset = 20;
+    var imgWidth = 320;
+    var imgHeight = 180;
+
+    // these 2 variable determine popup's distance from the cursor
+    // you might want to adjust to get the right result
+
+    /* END CONFIG */
+    $("#date-table td").hover(function (e) {
+        var imgAddrAttr = $(this).closest('td').attr('addr');
+        var imgNameAttr = $(this).closest('td').attr('name');
+        var fesChk = $("#fesImgConvertBtn").is(":checked");
+        if (fesChk == true) {
+            imgAddrAttr += "_f";
+        }
+
+        if (imgNameAttr != "" && imgNameAttr != undefined) {
+            $("body").append("<p id='preview'><img src='img/" + imgAddrAttr
+                + ".png' width='" + imgWidth + "' height='" + imgHeight + "'><br>" + imgNameAttr + "</p>");
+            $("#preview")
+                .css("top", (e.pageY - xOffset) + "px")
+                .css("left", (e.pageX + yOffset) + "px")
+                .fadeIn("fast");
+        }
+
+    },
+        function () {
+            $("#preview").remove();
+        });
+    $("#date-table td").mousemove(function (e) {
+        var previewWidth = $("#preview").width();
+        var previewHeight = $("#preview").height();
+
+        // console.log ((e.pageY + previewHeight) + ":" + ($(window).innerHeight() + $(document).scrollTop()))
+        if ((e.pageY + previewHeight) > ($(window).innerHeight() + $(document).scrollTop())) {
+            $("#preview").css("top", (e.pageY - xOffset - previewHeight) + "px")
+        }
+        else {
+            $("#preview").css("top", (e.pageY - xOffset) + "px")
+        }
+
+        // console.log ((e.pageX + previewWidth) + ":" + ($(window).innerWidth() + $(document).scrollLeft()))
+        if ((e.pageX + previewWidth) > ($(window).innerWidth() + $(document).scrollLeft())) {
+            $("#preview").css("left", (e.pageX - yOffset - previewWidth) + "px");
+        }
+        else {
+            $("#preview").css("left", (e.pageX + yOffset) + "px");
+        }
+
+        if ((e.pageY + previewHeight) > ($(window).innerHeight() + $(document).scrollTop()) &&
+            (e.pageX + previewWidth) > ($(window).innerWidth() + $(document).scrollLeft())) {
+            $("#preview")
+                .css("top", (e.pageY - xOffset - previewHeight - xOffset) + "px")
+                .css("left", (e.pageX - yOffset - previewWidth) + "px");
+        }
+    });
+}
+//////////////////////////////////////////////////
+
+function captureScreen(frameName) {
+
+    if (nowSelect != 0) {
+        var captureName;
+        var frameId;
+
+        if (frameName == 'TABLE') frameId = '#CAPTURE_FRAME'
+        else if (frameName == 'RANK') frameId = '#RANK'
+
+        if (nowSelect == 1) captureName = frameName + "_P_SSR.png";
+        else if (nowSelect == 2) captureName = frameName + "_S_SSR.png";
+        else if (nowSelect == 3) captureName = frameName + "_P_SR.png";
+        else if (nowSelect == 4) captureName = frameName + "_S_SR.png";
+
+        document.getElementById("convertSpan").style.display= 'none';
+
+        html2canvas(document.querySelector(frameId), { scrollY: -window.scrollY }).then(canvas => {
+            downloadURI(canvas.toDataURL('image/png'), captureName);
+        });
+
+        document.getElementById("convertSpan").style.display= '';
+    }
+}
+
+function downloadURI(uri, filename) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = uri;
+    link.click();
+}
